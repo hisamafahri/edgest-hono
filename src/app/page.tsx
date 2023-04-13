824b2e9854dcebc63a9a248f98ddf7cd731cc220
Hisam Fahri <hisam.fahri@pitik.id>
2023-04-13 18:23:33 +0700

    fix: check url on vercel & cloudflare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import { Inter } from "next/font/google";
 import { ApiType } from "@/pages/api/[...routes]";
 
 const client = hc<ApiType>(
-  process.env.NODE_ENV === "production"
-    ? process.env.VERCEL_URL || "http://127.0.0.1:3000"
+  process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.CF_PAGES_URL
+    ? `https://${process.env.CF_PAGES_URL}`
     : "http://127.0.0.1:3000"
 ).api;
 const inter = Inter({ subsets: ["latin"] });
